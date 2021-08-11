d80bdfa65d147e19767025f7b85efb54ae7ac861
Hemant Shrestha <sthahemant1st@gmail.com>
2021-08-11 17:33:16 +0545

    adds ui and socket codes

--- app/src/main/java/np/com/sthahemant1st/socketdemo/MainActivity.kt
+++ app/src/main/java/np/com/sthahemant1st/socketdemo/MainActivity.kt
@@ -1,38 +1,95 @@
 package np.com.sthahemant1st.socketdemo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import np.com.sthahemant1st.socketdemo.ui.theme.SocketDemoTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var webSocketClient: WebSocketClient
+    val socketListener = object : WebSocketClient.SocketListener {
+        override fun onMessage(message: String) {
+            Log.e("socketCheck onMessage", message)
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        webSocketClient = WebSocketClient.getInstance()
+        webSocketClient.setSocketUrl( "ws://echo.websocket.org")
+        webSocketClient.setListener(socketListener)
+        webSocketClient.connect()
+
         setContent {
             SocketDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MainScreen(
+                    onConnect = {
+                        webSocketClient.connect()
+                    },
+                    onDisconnect = {
+                        webSocketClient.disconnect()
+                    },
+                    onSendMessage = {
+                        webSocketClient.sendMessage(it)
+                    }
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen(onSendMessage: (String) -> Unit, onDisconnect: () -> Unit, onConnect: () -> Unit) {
+    var message by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp), verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Socket Demo", modifier = Modifier.weight(1f))
+
+        Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = message,
+                onValueChange = {
+                    message = it
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+            )
+            Button(modifier = Modifier.fillMaxWidth(), onClick = { onSendMessage(message) }) {
+                Text(text = "Send Message")
+            }
+            Button(modifier = Modifier.fillMaxWidth(), onClick = { onConnect() }) {
+                Text(text = "Connect Socket")
+            }
+            Button(modifier = Modifier.fillMaxWidth(), onClick = { onDisconnect() }) {
+                Text(text = "Disconnect Socket")
+            }
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SocketDemoTheme {
-        Greeting("Android")
+        MainScreen({},{},{})
     }
 }
--- app/src/main/java/np/com/sthahemant1st/socketdemo/WebSocketClient.kt
+++ app/src/main/java/np/com/sthahemant1st/socketdemo/WebSocketClient.kt
@@ -1,5 +1,6 @@
 package np.com.sthahemant1st.socketdemo
 
+import android.util.Log
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
@@ -7,49 +8,16 @@ import okhttp3.WebSocketListener
 
 class WebSocketClient {
     private lateinit var webSocket: okhttp3.WebSocket
-    var socketUrl = ""
-    var onReconnect: () -> Unit = {}
-    var shouldReconnect = true
-
-    private val webSocketListener = object : WebSocketListener() {
-        override fun onOpen(webSocket: okhttp3.WebSocket, response: Response) {
-            //called when connection succeeded
-            super.onOpen(webSocket, response)
-        }
-
-        override fun onMessage(webSocket: okhttp3.WebSocket, text: String) {
-            //called when text message received
-            super.onMessage(webSocket, text)
-        }
-
-        override fun onClosing(webSocket: okhttp3.WebSocket, code: Int, reason: String) {
-            //called when binary message received
-            super.onClosing(webSocket, code, reason)
-        }
-
-        override fun onClosed(webSocket: okhttp3.WebSocket, code: Int, reason: String) {
-            //called when no more messages and the connection should be released
-            super.onClosed(webSocket, code, reason)
-            if (shouldReconnect) onReconnect()
-        }
-
-        override fun onFailure(
-            webSocket: okhttp3.WebSocket, t: Throwable, response: Response?
-        ) {
-            super.onFailure(webSocket, t, response)
-//            loggerE("onFailure: localizedMessage=${t.localizedMessage}, response=${response?.body?.string()}")
-            if (shouldReconnect) onReconnect()
-        }
-    }
+    private var socketListener: SocketListener? = null
+    private var socketUrl = ""
+    private var shouldReconnect = true
+    private var client: OkHttpClient? = null
 
     companion object {
         private lateinit var instance: WebSocketClient
-
         @JvmStatic
         @Synchronized
-        /**
-         * This function gives singleton instance of WebSocket.
-         * */
+        //This function gives singleton instance of WebSocket.
         fun getInstance(): WebSocketClient {
             synchronized(WebSocketClient::class) {
                 if (!::instance.isInitialized) {
@@ -60,40 +28,97 @@ class WebSocketClient {
         }
     }
 
-    private fun initWebSocket() {
-        val request = Request.Builder().url(socketUrl).build()
-        webSocket = OkHttpClient().newWebSocket(request, webSocketListener)
+    fun setListener(listener: SocketListener) {
+        this.socketListener = listener
     }
 
-    fun reconnect() {
-        onReconnect()
-        shouldReconnect = true
+    fun setSocketUrl(socketUrl: String) {
+        this.socketUrl = socketUrl
+    }
+
+    private fun initWebSocket() {
+        client = OkHttpClient()
+        Log.e("socketCheck", "initWebSocket() socketurl = $socketUrl")
+        val request = Request.Builder().url(socketUrl).build()
+        webSocket = client!!.newWebSocket(request, webSocketListener)
+
+        client!!.dispatcher.executorService.shutdown()
     }
 
     fun connect() {
+        Log.e("socketCheck", "connect()")
+        shouldReconnect = true
         initWebSocket()
     }
 
-    fun sendMessage(message: String){
+    fun reconnect() {
+        Log.e("socketCheck", "reconnect()")
+        initWebSocket()
+    }
+
+    //send
+    fun sendMessage(message: String) {
+        Log.e("socketCheck", "sendMessage($message)")
         webSocket.send(message)
     }
 
-    /*
-    *We can close socket by two way:
-    *
-    * 1. websocket.webSocket.close(1000, "Dont need connection")
-    *This attempts to initiate a graceful shutdown of this web socket.
-    *Any already-enqueued messages will be transmitted before the close message is sent but
-    *subsequent calls to send will return false and their messages will not be enqueued.
-    *
-    * 2. websocket.cancel()
-    * This immediately and violently release resources held by this web socket,
-    *  discarding any enqueued messages.
-    *
-    * Both does nothing if the web socket has already been closed or canceled.
-    * */
+
+    //We can close socket by two way:
+
+    //1. websocket.webSocket.close(1000, "Dont need connection")
+    //This attempts to initiate a graceful shutdown of this web socket.
+    //Any already-enqueued messages will be transmitted before the close message is sent but
+    //subsequent calls to send will return false and their messages will not be enqueued.
+
+    //2. websocket.cancel()
+    //This immediately and violently release resources held by this web socket,
+    //discarding any enqueued messages.
+
+    //Both does nothing if the web socket has already been closed or canceled.
     fun disconnect() {
         webSocket.close(1000, "Do not need connection anymore.")
         shouldReconnect = false
+    }
+
+    interface SocketListener {
+        fun onMessage(message: String)
+    }
+
+
+    private val webSocketListener = object : WebSocketListener() {
+        //called when connection succeeded
+        //we are sending a message just after the socket is opened
+        override fun onOpen(webSocket: okhttp3.WebSocket, response: Response) {
+//            webSocket.send("Initial message form onOpen()")
+            Log.e("socketCheck", "onOpen()")
+//            super.onOpen(webSocket, response)
+        }
+
+        //called when text message received
+        override fun onMessage(webSocket: okhttp3.WebSocket, text: String) {
+            socketListener?.onMessage(text)
+//            super.onMessage(webSocket, text)
+        }
+
+        //called when binary message received
+        override fun onClosing(webSocket: okhttp3.WebSocket, code: Int, reason: String) {
+            Log.e("socketCheck", "onClosing()")
+//            super.onClosing(webSocket, code, reason)
+        }
+
+        override fun onClosed(webSocket: okhttp3.WebSocket, code: Int, reason: String) {
+            //called when no more messages and the connection should be released
+            Log.e("socketCheck", "onClosed()")
+//            super.onClosed(webSocket, code, reason)
+            if (shouldReconnect) reconnect()
+        }
+
+        override fun onFailure(
+            webSocket: okhttp3.WebSocket, t: Throwable, response: Response?
+        ) {
+            Log.e("socketCheck", "onFailure()")
+            if (shouldReconnect) reconnect()
+//            super.onFailure(webSocket, t, response)
+        }
     }
 }
